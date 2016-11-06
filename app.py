e383ea0518b78e6325db6e042e1becba65215876
Liza Daly <lizadaly@gmail.com>
2016-11-06 15:42:52 -0500

    Good blocking

--- app.py
+++ app.py
@@ -26,18 +26,29 @@ def draw_horizontal_lines(draw, boxes, line_height, line_spacing):
         # Note: this will clobber boxes on the same line, OK for now
         skip_line = False
 
-        for box in boxes:
-            if y >= box.position[0][1] and y + line_height + line_spacing <= box.position[1][1]:
+        for i, b in enumerate(boxes):
+            avoid_box = None
+            last_box = boxes[i-1]
+            this_box = boxes[i]
+            for box in (this_box, last_box):
+                if box.position[0][1] > y - (gridline_height * 1.2) and box.position[1][1] < y + (gridline_height * 1.2):
+#                    box.position[1][1] > y and box.position[1][1] < y + (gridline_height * 2):
+                    avoid_box = box
+                    break
+            if avoid_box:
                 # This is the line we're on, so we need two lines: before and after
                 start_x = 0
-                end_x = box.position[0][0]
+                end_x = avoid_box.position[0][0]
                 draw_line(draw, [start_x, start_y, end_x, start_y], line_height=line_height)
-                start_x = box.position[1][0]
+                start_x = avoid_box.position[1][0]
                 end_x = img.size[0]
                 draw_line(draw, [start_x, start_y, end_x, start_y], line_height=line_height)
                 skip_line = True
         if not skip_line:
             draw_line(draw, [start_x, start_y, end_x, start_y], line_height=line_height)
+
+    for box in boxes:
+        draw.rectangle(box.position, outline='#ff0000')
 
 #draw.rectangle(box.position)#, fill=255)
 def draw_line(draw, pos, line_height):
@@ -71,5 +82,7 @@ if __name__ == '__main__':
 
     img = Image.open(imagefile)
     draw = ImageDraw.Draw(img)
-    draw_horizontal_lines(draw, boxes, line_height=line_height, line_spacing=line_spacing)
+
+    select_boxes = [boxes[10], boxes[20], boxes[30]]
+    draw_horizontal_lines(draw, select_boxes, line_height=line_height, line_spacing=line_spacing)
     img.save("out.png")
